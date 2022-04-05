a6382573b9b9b0f1d66e6597337cac82a9e27188
Laura Gil <a25589svalero.com>
2022-04-05 20:33:35 +0200

    Atributos, getters, setters y constructores creados

---
+++ ProyectoEjemplo/src/RetoV0/Producto.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package RetoV0;
+
+/**
+ *
+ * @author laura
+ */
+public class Producto {
+    
+    //Atributos
+    private static int idProducto;
+    private String nombreProd;
+    private String descriptProd;
+    private String medidasProd;
+    private int stockProd;
+    
+    //Getters
+    public static int getIdProducto() {
+        return idProducto;
+    }
+
+    public static String getNombreProd() {
+        return nombreProd;
+    }
+
+    public static String getDescriptProd() {
+        return descriptProd;
+    }
+
+    public static String getMedidasProd() {
+        return medidasProd;
+    }
+
+    public static int getStockProd() {
+        return stockProd;
+    }
+
+    
+    
+
+    //Setters
+    public static void setIdProducto(int idProducto) {
+        Producto.idProducto = idProducto;
+    }
+
+    public static void setNombreProd(String nombreProd) {
+        Producto.nombreProd = nombreProd;
+    }
+
+    public static void setDescriptProd(String descriptProd) {
+        Producto.descriptProd = descriptProd;
+    }
+
+    public static void setMedidasProd(String medidasProd) {
+        Producto.medidasProd = medidasProd;
+    }
+
+    public static void setStockProd(int stockProd) {
+        Producto.stockProd = stockProd;
+    }
+    
+    
+    //Constructore sin tener en cuenta el stock, por defecto hay 1 producto
+    public Producto(int idProducto, String nombreProd, String descriptProd, String medidasProd) {
+        this.idProducto = idProducto;
+        this.nombreProd = nombreProd;
+        this.descriptProd = descriptProd;
+        this.medidasProd = medidasProd;
+        this.stockProd = 1;
+    }
+    
+    //Constructore teniendo en cuenta el stock
+    public Producto(int idProducto, String nombreProd, String descriptProd, String medidasProd, int stock) {
+        this.idProducto = idProducto;
+        this.nombreProd = nombreProd;
+        this.descriptProd = descriptProd;
+        this.medidasProd = medidasProd;
+        this.stockProd = stock;
+    }
+    
+    
+    //Métodos
+    
+    
+
+    
+    
+        
+    
+    
+}
