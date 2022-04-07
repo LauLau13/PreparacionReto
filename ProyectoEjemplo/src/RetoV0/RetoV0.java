b1d69912e32d0e444a795aa86a74a6ede0b9d604
Alexander Aguirre <keevinaguirre@gmail.com>
2022-04-07 18:11:37 +0200

    Revert "Merge branch 'feature/UserClass' into develop"
    This reverts commit 5327cc483251c33c33eae46154e4230bd78dc4c1, reversing
    changes made to 06226b74a51b2255731b0d5613522688775ff611.
    
    Reverted the merge

--- ProyectoEjemplo/src/RetoV0/RetoV0.java
+++ ProyectoEjemplo/src/RetoV0/RetoV0.java
@@ -19,45 +19,38 @@ public class RetoV0 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-        /*
-         * try{
-         * //1.Descargar e instalar el driver
-         * //2.Agregar el driver al proyecto
-         * //Libraries add jar forlder
-         * Class.forName("oracle.jdbc.OracleDriver");
-         * 
-         * //3.Crear una connexión
-         * String cadenaConexion = "jdbc:oracle:thin:@localhost:1521/XE";
-         * Connection conexion = DriverManager.getConnection(cadenaConexion, "ejemplo",
-         * "ejemplo");
-         * 
-         * //4. Crear una sentencia
-         * Statement stmt = conexion.createStatement();
-         * 
-         * //5. Generar la query que es una consulta (borrado, update, insert...)
-         * String query = "INSERT INTO personas (dni, nombre, apellido, edad)"
-         * + "VALUES (766666, 'Laura', 'Perez', 22)";
-         * 
-         * //6. Ejecutar la sentencia sql que devuelve un resultset
-         * ResultSet rs = stmt.executeQuery(query);
-         * 
-         * //7. Manejar los resultados: resultset y métodos getX
-         * 
-         * //8. Cerrar la conexión y liberar todos los reculrsos
-         * stmt.close();
-         * conexion.close();
-         * //Comprobar en administrador de tareas, sevices si OracleServceXE
-         * 
-         * }catch(Exception e){
-         * System.out.println(e.getMessage());
-         * }
-         */
-
-        // Prueba de Detalle de usuario
-        Usuarios u1 = new Usuarios("Alexander", "Aguirre", "keevinaguirre@gmail.com", "1234");
-        u1.setDireccion("Av. Cesareo Alierta 78");
-        System.out.println(u1.toString());
+        
+        try{
+            //1.Descargar e instalar el driver
+            //2.Agregar el driver al proyecto
+                //Libraries add jar forlder
+                Class.forName("oracle.jdbc.OracleDriver");
+            
+            //3.Crear una connexión
+                String cadenaConexion = "jdbc:oracle:thin:@localhost:1521/XE";
+                Connection conexion = DriverManager.getConnection(cadenaConexion, "ejemplo", "ejemplo");
+            
+            //4. Crear una sentencia
+                Statement stmt = conexion.createStatement();
+                
+            //5. Generar la query que es una consulta (borrado, update, insert...)
+                String query = "INSERT INTO personas (dni, nombre, apellido, edad)"
+                        + "VALUES (766666, 'Laura', 'Perez', 22)";
+            
+            //6. Ejecutar la sentencia sql que devuelve un resultset
+                ResultSet rs = stmt.executeQuery(query);
+            
+            //7. Manejar los resultados: resultset y métodos getX
+            
+            //8. Cerrar la conexión y liberar todos los reculrsos
+                stmt.close();
+                conexion.close();
+                //Comprobar en administrador de tareas, sevices si OracleServceXE
+                
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+            
     }
-
+    
 }
