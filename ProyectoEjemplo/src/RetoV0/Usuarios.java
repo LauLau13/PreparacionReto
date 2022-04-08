e137db6455c061088a8a231c5d349c09b621c205
Alexander Aguirre Mora <aaguirrem@hiberus.com>
2022-04-08 19:22:07 +0200

    Actualizado nombre de variables para match con los campos SQL

--- ProyectoEjemplo/src/RetoV0/Usuarios.java
+++ ProyectoEjemplo/src/RetoV0/Usuarios.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Usuarios {
     // Atributos:
     private static int idUsuario = 0;
-    private String nombre;
-    private String apellido;
-    private String email;
+    private String nombreUsuario;
+    private String apellidoUsuario;
+    private String emailUsuario;
     private String password;
     private String direccion;
     private boolean esAdmin = false;
@@ -15,20 +15,24 @@ public class Usuarios {
     // Constructor de usuarios normales
     // Con dirección
 
-    public Usuarios(String nombre, String apellido, String email, String password, String direccion) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.email = email;
+    public Usuarios(String nombreUsuario, String apellidoUsuario, String emailUsuario, String password, String direccion) {
+        this.nombreUsuario = nombreUsuario;
+        this.apellidoUsuario = apellidoUsuario;
+        this.emailUsuario
+ = emailUsuario
+;
         this.password = password;
         this.direccion = direccion;
         idUsuario++;
     }
 
     // Sin dirección
-    public Usuarios(String nombre, String apellido, String email, String password) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.email = email;
+    public Usuarios(String nombreUsuario, String apellidoUsuario, String emailUsuario, String password) {
+        this.nombreUsuario = nombreUsuario;
+        this.apellidoUsuario = apellidoUsuario;
+        this.emailUsuario
+ = emailUsuario
+;
         this.password = password;
         idUsuario++;
     }
@@ -36,10 +40,12 @@ public class Usuarios {
     // Constructor de admins
     // Con dirección
 
-    public Usuarios(String nombre, String apellido, String email, String password, String direccion, boolean esAdmin) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.email = email;
+    public Usuarios(String nombreUsuario, String apellidoUsuario, String emailUsuario, String password, String direccion, boolean esAdmin) {
+        this.nombreUsuario = nombreUsuario;
+        this.apellidoUsuario = apellidoUsuario;
+        this.emailUsuario
+ = emailUsuario
+;
         this.password = password;
         this.direccion = direccion;
         this.esAdmin = esAdmin;
@@ -47,10 +53,12 @@ public class Usuarios {
     }
 
     // Sin dirección
-    public Usuarios(String nombre, String apellido, String email, String password, boolean esAdmin) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.email = email;
+    public Usuarios(String nombreUsuario, String apellidoUsuario, String emailUsuario, String password, boolean esAdmin) {
+        this.nombreUsuario = nombreUsuario;
+        this.apellidoUsuario = apellidoUsuario;
+        this.emailUsuario
+ = emailUsuario
+;
         this.password = password;
         this.esAdmin = esAdmin;
         idUsuario++;
@@ -65,7 +73,8 @@ public class Usuarios {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("¿Qué datos deseas cambiar?");
-        System.out.println("1. Nombre \n 2. Apellido \n 3. Email \n 4. Contraseña \n 5. Dirección \n 6.Salir");
+        System.out.println("1. Nombre \n 2. Apellido \n 3. emailUsuario
+ \n 4. Contraseña \n 5. Dirección \n 6.Salir");
         int opcion = sc.nextInt();
 
         while (opcion != 6) {
@@ -77,7 +86,8 @@ public class Usuarios {
                     // Modificación del apellido en la ddbb
                     break;
                 case 3:
-                    // Modificación del Email en la ddbb
+                    // Modificación del emailUsuario
+             en la ddbb
                     break;
                 case 4:
                     System.out.println("Introduce tu contraseña actual: ");
@@ -105,7 +115,9 @@ public class Usuarios {
     @Override
     public String toString() {
         return "Bienvenido " + this.nombre + ", estos son tus datos: " + "\n" + "Nombre Completo: " + this.nombre + " "
-                + this.apellido + "\n" + "Email: " + this.email + "\n" + "Dirección: " + this.direccion;
+                + this.apellidoUsuario + "\n" + "emailUsuario
+        : " + this.emailUsuario
+         + "\n" + "Dirección: " + this.direccion;
     }
 
 }
