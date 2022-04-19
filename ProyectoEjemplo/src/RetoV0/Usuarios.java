b476fe22b6f6decdb780ffe13d90bdd4de47399f
Alexander Aguirre Mora <aaguirrem@hiberus.com>
2022-04-19 18:25:29 +0200

    Prueba conexion a base de datos

--- ProyectoEjemplo/src/RetoV0/Usuarios.java
+++ ProyectoEjemplo/src/RetoV0/Usuarios.java
@@ -1,10 +1,17 @@
 package RetoV0;
 
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class Usuarios {
     // Atributos:
-    private static int idUsuario = 0;
+    private int idUsuario = 0;
     private String nombreUsuario;
     private String apellidoUsuario;
     private String emailUsuario;
@@ -12,17 +19,20 @@ public class Usuarios {
     private String direccion;
     private boolean esAdmin = false;
 
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
     // Constructor de usuarios normales
     // Con dirección
 
-    public Usuarios(String nombreUsuario, String apellidoUsuario, String emailUsuario, String password, String direccion) {
+    public Usuarios(String nombreUsuario, String apellidoUsuario, String emailUsuario, String password,
+            String direccion) {
         this.nombreUsuario = nombreUsuario;
         this.apellidoUsuario = apellidoUsuario;
-        this.emailUsuario
- = emailUsuario
-;
+        this.emailUsuario = emailUsuario;
         this.password = password;
         this.direccion = direccion;
+        this.consultaNewUser();
         idUsuario++;
     }
 
@@ -30,38 +40,70 @@ public class Usuarios {
     public Usuarios(String nombreUsuario, String apellidoUsuario, String emailUsuario, String password) {
         this.nombreUsuario = nombreUsuario;
         this.apellidoUsuario = apellidoUsuario;
-        this.emailUsuario
- = emailUsuario
-;
+        this.emailUsuario = emailUsuario;
         this.password = password;
+        this.consultaNewUser();
         idUsuario++;
     }
 
     // Constructor de admins
     // Con dirección
 
-    public Usuarios(String nombreUsuario, String apellidoUsuario, String emailUsuario, String password, String direccion, boolean esAdmin) {
+    public Usuarios(String nombreUsuario, String apellidoUsuario, String emailUsuario, String password,
+            String direccion, boolean esAdmin) {
         this.nombreUsuario = nombreUsuario;
         this.apellidoUsuario = apellidoUsuario;
-        this.emailUsuario
- = emailUsuario
-;
+        this.emailUsuario = emailUsuario;
         this.password = password;
         this.direccion = direccion;
         this.esAdmin = esAdmin;
+        this.consultaNewUser();
         idUsuario++;
     }
 
     // Sin dirección
-    public Usuarios(String nombreUsuario, String apellidoUsuario, String emailUsuario, String password, boolean esAdmin) {
+    public Usuarios(String nombreUsuario, String apellidoUsuario, String emailUsuario, String password,
+            boolean esAdmin) {
         this.nombreUsuario = nombreUsuario;
         this.apellidoUsuario = apellidoUsuario;
-        this.emailUsuario
- = emailUsuario
-;
+        this.emailUsuario = emailUsuario;
         this.password = password;
         this.esAdmin = esAdmin;
+        this.consultaNewUser();
         idUsuario++;
+    }
+
+    /**
+     * Consulta predeterminada para añadir cada Objeto de tipo Usuario creado a la
+     * tabla realacional de la base de datos
+     */
+    public void consultaNewUser() {
+        try {
+
+            Class.forName("oracle.jdbc.OracleDriver");
+
+            String cadenaConexion = "jdbc:oracle:thin:@localhost:1521/XE";
+
+            Connection conexion = DriverManager.getConnection(cadenaConexion, "RETOJAVA", "RETOJAVA");
+
+            String query = "INSERT INTO USUARIOS VALUES(? ,? ,? ,? ,? ,? ,? )";
+
+            PreparedStatement createUser = conexion.prepareStatement(query);
+
+            createUser.setInt(1, this.idUsuario);
+            createUser.setString(2, this.nombreUsuario);
+            createUser.setString(3, this.apellidoUsuario);
+            createUser.setString(4, this.emailUsuario);
+            createUser.setString(5, this.password);
+            createUser.setString(6, this.direccion);
+            createUser.setBoolean(7, this.esAdmin);
+
+            createUser.close();
+        } catch (ClassNotFoundException cn) {
+            cn.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -71,53 +113,82 @@ public class Usuarios {
      */
     public void cambiarDatos() {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("¿Qué datos deseas cambiar?");
-        System.out.println("1. Nombre \n 2. Apellido \n 3. emailUsuario
- \n 4. Contraseña \n 5. Dirección \n 6.Salir");
-        int opcion = sc.nextInt();
+        try {
 
-        while (opcion != 6) {
-            switch (opcion) {
-                case 1:
-                    // Modificación del nombre en la ddbb
-                    break;
-                case 2:
-                    // Modificación del apellido en la ddbb
-                    break;
-                case 3:
-                    // Modificación del emailUsuario
-             en la ddbb
-                    break;
-                case 4:
-                    System.out.println("Introduce tu contraseña actual: ");
-                    String passIntroducida = sc.nextLine();
-                    if (passIntroducida.equals(this.password)) {
-                        // Modificar la contraseña
-                    } else {
-                        System.out.println("Has introducido la contraseña equivocada");
-                    }
-                    break;
-                case 5:
-                    // Cambiar dirección
-                    break;
-                default:
-                    break;
+            Class.forName("oracle.jdbc.OracleDriver");
+
+            String cadenaConexion = "jdbc:oracle:thin:@localhost:1521/XE";
+
+            Connection conexion = DriverManager.getConnection(cadenaConexion, "RETOJAVA", "RETOJAVA");
+
+            String query = "";
+            PreparedStatement modifyField = conexion.prepareStatement(query);
+            ;
+
+            Statement commit = conexion.createStatement();
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("¿Qué datos deseas cambiar?");
+            System.out
+                    .println("1. Nombre \n 2. Apellido \n 3. emailUsuario \n 4. Contraseña \n 5. Dirección \n 6.Salir");
+            int opcion = sc.nextInt();
+
+            while (opcion != 6) {
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Introduzca el nuevo nombre: ");
+                        String newName = sc.nextLine();
+                        // Modificación del nombre en la ddbb
+                        query = "UPDATE USUARIOS SET nombreUsuario = ? where idUsuario = ?";
+                        modifyField = conexion.prepareStatement(query);
+                        modifyField.setString(1, newName);
+                        modifyField.setInt(2, this.idUsuario);
+                        modifyField.executeQuery();
+                        commit.executeQuery("Commit");
+                        break;
+                    case 2:
+                        // Modificación del apellido en la ddbb
+                        break;
+                    case 3:
+                        // Modificación del emailUsuario en la ddbb
+                        break;
+                    case 4:
+                        System.out.println("Introduce tu contraseña actual: ");
+                        String passIntroducida = sc.nextLine();
+                        if (passIntroducida.equals(this.password)) {
+                            // Modificar la contraseña
+                        } else {
+                            System.out.println("Has introducido la contraseña equivocada");
+                        }
+                        break;
+                    case 5:
+                        // Cambiar dirección
+                        break;
+                    default:
+
+                        break;
+                }
             }
+
+            modifyField.close();
+        } catch (ClassNotFoundException cn) {
+            cn.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
     }
 
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
     @Override
     public String toString() {
-        return "Bienvenido " + this.nombre + ", estos son tus datos: " + "\n" + "Nombre Completo: " + this.nombre + " "
-                + this.apellidoUsuario + "\n" + "emailUsuario
-        : " + this.emailUsuario
-         + "\n" + "Dirección: " + this.direccion;
+        return "Bienvenido " + this.nombreUsuario
+                + ", estos son tus datos: "
+                + "\n"
+                + "Nombre Completo: " + this.nombreUsuario + " " + this.apellidoUsuario
+                + "\n"
+                + "emailUsuario: " + this.emailUsuario
+                + "\n"
+                + "Dirección: " + this.direccion;
     }
 
 }
